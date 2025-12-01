6f0df11ff9a6caacff96cecd97061538462a5abb
egoloprum <bayraa093@gmail.com>
2025-12-01 17:57:47 +0300

    settings: tailwind is added

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function Index() {
+  return (
+    <View className="flex justify-center items-center bg-red-500 h-full">
+      <Text>hello world</Text>
+    </View>
+  )
+}
